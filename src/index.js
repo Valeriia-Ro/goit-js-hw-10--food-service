715a9c432c2977c6edca4e90bb39d3207ec6d510
Valeriia-Ro <vkolchak17@gmail.com>
2021-04-07 15:48:53 +0300

    внесла  изменения

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 // import menuElements from '../src/menu.json';
-// import foodItem from '../fooditems.hbs';
+// import foodItem from '../src/fooditems.hbs';
 
 
 const Theme = {
@@ -12,35 +12,38 @@ const menu = document.querySelector('.js-menu');
 const bodyRef = document.querySelector('body');
 const btn = document.querySelector('#theme-switch-toggle');
 
-// const menu = document.querySelector('.js-menu');
-// const cardsMarkup =  createCardElement(menuElement);
 
-// function createCardElement(menuElement) {
-//     return menuElement.map(skeletonMenuEl).join('');
+// const cardsMarkup =  createCardElement(menuElements);
+
+// function createCardElement(menuElements) {
+//     return menuElement.map(foodItem).join('');
 // }
 
 // menu.insertAdjacentHTML('beforeend',cardsMarkup);
 
 bodyRef.classList.add(Theme.LIGHT);
-const string = localStorage.setItem('Theme', JSON.stringify(Theme));
+
+localStorage.setItem('theme', JSON.stringify(Theme));
+const string = localStorage.getItem('theme');
+
 
 btn.addEventListener('change',changeThemeColor);
 
 function changeClass(addClass, remClass) {
     bodyRef.classList.remove(remClass);
     bodyRef.classList.add(addClass);
-}
+};
 
 function darkTheme () {
     changeClass(Theme.DARK, Theme.LIGHT);
     localStorage.setItem('Theme', Theme.DARK);
-    checkRef.setAttribute('checked', true)
-}
+    btn.setAttribute('checked', true)
+};
 
 function lightTheme () {
     changeClass(Theme.LIGHT, Theme.DARK);
     localStorage.setItem('Theme', Theme.LIGHT);
-}
+};
 
 function changeThemeColor() {
 
@@ -50,4 +53,5 @@ function changeThemeColor() {
         lightTheme();
     };
 
-}
+};
+
